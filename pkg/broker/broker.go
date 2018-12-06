201b0897e81daf8b7acdce94ffd074936f9ae0f5
edisonxiang <xiang.edison@gmail.com>
2018-12-06 15:15:20 +0800

    fix accepts_incomplete in update

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -134,10 +134,12 @@ func (cloudBroker *CloudServiceBroker) Provision(
 		return brokerapi.ProvisionedServiceSpec{}, error
 	}
 
-	/*e := cloudBroker.Catalog.ValidateAcceptsIncomplete(asyncAllowed)
-	if e != nil {
-		return brokerapi.ProvisionedServiceSpec{}, e
-	}*/
+	if details.ServiceID == "" || details.PlanID == "" {
+		e := cloudBroker.Catalog.ValidateAcceptsIncomplete(asyncAllowed)
+		if e != nil {
+			return brokerapi.ProvisionedServiceSpec{}, e
+		}
+	}
 
 	// find service plan
 	_, err := cloudBroker.Catalog.FindServicePlan(details.ServiceID, details.PlanID)
@@ -164,10 +166,12 @@ func (cloudBroker *CloudServiceBroker) Deprovision(
 
 	cloudBroker.Logger.Debug(fmt.Sprintf("Deprovision received. instanceID: %s", instanceID))
 
-	/*e := cloudBroker.Catalog.ValidateAcceptsIncomplete(asyncAllowed)
-	if e != nil {
-		return brokerapi.DeprovisionServiceSpec{}, e
-	}*/
+	if details.ServiceID == "" || details.PlanID == "" {
+		e := cloudBroker.Catalog.ValidateAcceptsIncomplete(asyncAllowed)
+		if e != nil {
+			return brokerapi.DeprovisionServiceSpec{}, e
+		}
+	}
 
 	// find service plan
 	_, err := cloudBroker.Catalog.FindServicePlan(details.ServiceID, details.PlanID)
@@ -244,10 +248,12 @@ func (cloudBroker *CloudServiceBroker) Update(
 
 	cloudBroker.Logger.Debug(fmt.Sprintf("Update received. instanceID: %s", instanceID))
 
-	/*e := cloudBroker.Catalog.ValidateAcceptsIncomplete(asyncAllowed)
-	if e != nil {
-		return brokerapi.UpdateServiceSpec{}, e
-	}*/
+	if details.ServiceID == "" || details.PlanID == "" {
+		e := cloudBroker.Catalog.ValidateAcceptsIncomplete(asyncAllowed)
+		if e != nil {
+			return brokerapi.UpdateServiceSpec{}, e
+		}
+	}
 
 	// find service plan
 	_, err := cloudBroker.Catalog.FindServicePlan(details.ServiceID, details.PlanID)
