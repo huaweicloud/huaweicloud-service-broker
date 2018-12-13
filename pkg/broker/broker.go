b0101f9ba6110c02eaeb0eebd9d2fdf1e1740ef0
edisonxiang <xiang.edison@gmail.com>
2018-12-13 17:08:13 +0800

    fix planid is empty when updated

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -253,6 +253,36 @@ func (cloudBroker *CloudServiceBroker) Update(
 		if e != nil {
 			return brokerapi.UpdateServiceSpec{}, e
 		}
+
+		if instanceID != "" {
+			// Check instance length in back database
+			var length int
+			err := database.BackDBConnection.
+				Model(&database.InstanceDetails{}).
+				Where("instance_id = ?", instanceID).
+				Count(&length).Error
+			if err != nil {
+				return brokerapi.UpdateServiceSpec{}, err
+			}
+			// instance is existing
+			if length > 0 {
+				// Get InstanceDetails in back database
+				iddetail := database.InstanceDetails{}
+				err = database.BackDBConnection.
+					Where("instance_id = ?", instanceID).
+					First(&iddetail).Error
+				if err != nil {
+					return brokerapi.UpdateServiceSpec{}, err
+				}
+
+				details.ServiceID = iddetail.ServiceID
+				details.PlanID = iddetail.PlanID
+
+				cloudBroker.Logger.Debug(
+					fmt.Sprintf("Auto discovery ServiceID: %s and PlanID: %s for instanceID: %s",
+						details.ServiceID, details.PlanID, instanceID))
+			}
+		}
 	}
 
 	// find service plan
